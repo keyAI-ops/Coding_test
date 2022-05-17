96d5a1bece4874e3c810e579e51979874aeff303
Chang_K <ckckckck1207@naver.com>
2022-05-17 23:42:40 +0900

    coding_test_prime_num

--- Programers_Lv1/create_prime_number.py
+++ Programers_Lv1/create_prime_number.py
@@ -6,10 +6,25 @@ def solution(nums):
     sum_list = [sum(i) for i in a_list if sum(i)%2 != 0]
     
     for i in sum_list:
-        f_list = []
+        fail_list = []
         for j in range(3,i,2):
             if i%j == 0:
-                f_list.append(j)
-        if len(f_list) == 0:
+                fail_list.append(j)
+        if len(fail_list) == 0:
             result += 1
     return result
+
+''' 다른 사람의 풀이
+def solution(nums):
+    from itertools import combinations as cb
+    answer = 0
+    for a in cb(nums, 3):
+        cand = sum(a)
+        for j in range(2, cand):
+            if cand%j==0:
+                break
+        else:
+            answer += 1
+    return answer
+
+'''
